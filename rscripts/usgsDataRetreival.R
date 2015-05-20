2fdf11acd047c8e918dec2f4502ca5ee576144d0
mjpdenver <matt_pocernich@hotmail.com>
2015-05-20 11:59:41 -0600

    More stuff

--- rscripts/usgsDataRetreival.R
+++ rscripts/usgsDataRetreival.R
@@ -5,72 +5,80 @@
 # install.packages("dataRetrieval")
 
 library(dataRetrieval)
+library(reshape2)
 
-# Choptank River near Greensboro, MD:
-siteNumber <- "01491000"
-parameterCd <- "00060" # Discharge
-startDate <- "2009-10-01"
-endDate <- "2012-09-30"
-
+## roaring fork sites
 
 rf_sites <- c("09072550", "09073005", "09073300", "09073400", "09074000", "09074500", "09075400", "09078141", "09078475", "09079450", "09080400", "09081000", "09081600", "09085000")
 
-#http://waterdata.usgs.gov/nwis/current?huc_cd=14010004&sitefile_output_format=html_table&PARAmeter_cd=STATION_NM
+# source
+# http://waterdata.usgs.gov/nwis/current?huc_cd=14010004&sitefile_output_format=html_table&PARAmeter_cd=STATION_NM
 
-discharge <- readNWISdv(siteNumber = rf_sites,
-                        parameterCd, startDate, endDate)
+
 
 ### example of XML query, contains attribute information.
-startDate <- "1990-09-01"
+startDate <- "2000-01-01"
 endDate <- "2014-10-01"
 offering <- '00003'
 property <- '00060'
 
+#discharge <- readNWISdv(siteNumber = rf_sites,
+#                        parameterCd, startDate, endDate)
+
 obs_url <- constructNWISURL(rf_sites,property,startDate,endDate,'dv')
 
-data <- importWaterML1(obs_url)
-
-usgs_site_info <- attr(data, which="siteInfo")
-names(usgs_site_info)[6:7] <- c("latitude", "longitude")
-
-
-### map and compare with monitoring sites
-
-dat   <- read.csv("../data/dsn_pull/rf-1990-2015.csv", header = TRUE, as.is = TRUE)
-
-sites <- read.csv("../data/dsn_pull/rf_sites.csv", header = TRUE, as.is = TRUE )
-
-dat$date <- as.Date(dat$Activity.Start.Date, format = "%m/%d/%Y")
-dat$Result.Value <- as.numeric(dat$Result.Value)
-dat_dt <- as.tbl(dat)
-
-
-events <- unique( dat[ , names(dat) %in% c("Activity.ID", "date", "Monitoring.Location.ID", "Monitoring.Location.Name", "Monitoring.Location.Latitude", "Monitoring.Location.Longitude"   ) ] )       
-events <- as.tbl(events)
-
-main_sites <- filter(events,date >= as.Date("1/1/2000", format = "%m/%d/%Y") )  %>% group_by(Monitoring.Location.ID,Monitoring.Location.Name, Monitoring.Location.Latitude, Monitoring.Location.Longitude  )%>% 
-    summarize( n_events = n_distinct(date),
-               earliest = min(date), 
-               latest = max(date)) %>% 
-    arrange( desc(n_events)) %>%  filter( n_events > 25 )
-
-names(main_sites)[3:4] <- c("latitude", "longitude")
-
-
-
-dw_sites <- unique( dat[ , names(dat) %in% c("Monitoring.Location.ID", "Monitoring.Location.Name" ) ] )  
-
-
-### map
-
-m = leaflet() %>% addTiles()
-m %>% addCircles(data=main_sites) %>% 
-    addCircles(data = usgs_site_info,color = "red",fill = "red") %>% 
-    addMarkers(data=usgs_site_info, popup=usgs_site_info$site_no )
+data <- importWaterML1(obs_url,asDateTime = TRUE)
 
 usgs_dat <- as.tbl(data)
 names(usgs_dat)[6] <- "flow_cfs"
 
-temp <- filter(usgs_dat, site_no %in% c("09085000", "09081600", "09081000", "09080400", "09073400" ) )
-library(reshape2)
-flow_wide <- dcast(data = temp, formula = dateTime ~site_no, value.var =  "flow_cfs"  )
+usgs_dat <- as.tbl(data)
+names(usgs_dat)[6] <- "flow_cfs"
+
+## a subset of sites on major branches selected visually.
+
+# temp <- filter(usgs_dat, site_no %in% c("09085000", "09081600", "09081000", "09080400", "09073400" ) ) 
+flow_wide <- dcast(data = usgs_dat, formula = dateTime ~site_no, value.var =  "flow_cfs"  )
+
+## chose sites with few missing values
+ii <- apply(flow_wide, 2, function(x){sum(is.na(x)) } ) < 2  ## 
+
+flow_wide <- flow_wide[,ii]
+
+cor(flow_wide[, -1],use = "pairwise.complete.obs")  ## also drop site 09080400 because it is below Ruedi Res and is not well correlated with others.  Add caveat.
+
+flow_wide <- flow_wide[, names(flow_wide)!= "09080400"]
+flow_wide$dateTime <- as.Date(flow_wide$dateTime)
+
+names(flow_wide)[1] <- "date"
+
+if(FALSE){
+    dygraph(flow_wide[,1:2])
+
+    
+}
+flow_scaled <- flow_wide
+
+for(i in 2:ncol(flow_wide) ){
+    flow_scaled[,i] <- scale(flow_wide[,i])
+}
+
+flow_scaled$avg <- apply(flow_scaled[,-1],1, mean )
+
+ff <- ecdf( flow_scaled$avg)
+flow_scaled$ecdf <- ff(flow_scaled$avg)
+flow_scaled$glenwood <- flow_wide[, names(flow_wide)=="09085000" ]
+
+## keep in mind that ecdf scaling cannot be transposed back to flow units.
+## wq
+
+#obs_url <- constructNWISURL(rf_sites,"00910",startDate,endDate,'qw')
+#wq_data    <- importWaterML1(obs_url)
+
+
+usgs_site_info <- attr(data, which="siteInfo")
+names(usgs_site_info)[6:7] <- c("latitude", "longitude")
+usgs_site_info$major <- usgs_site_info$site_no %in% names(flow_wide)
+
+
+save( list = c("usgs_site_info", "flow_wide"), file = "../data/binaryFlow.Rdata")
